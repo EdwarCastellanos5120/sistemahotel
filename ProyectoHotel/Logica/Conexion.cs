68285beaca4bb6a457d4f682b476c6ea621b35d1
EdwarCastellanos5120 <ecastellanosp1@miumg.edu.gt>
2023-10-26 19:04:40 -0600

    Subiendo

---
+++ ProyectoHotel/Logica/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoHotel.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "workstation id=DB_HOTEL_4565.mssql.somee.com;packet size=4096;user id=hotelhosting_SQLLogin_2;pwd=fbb6luf9jr;data source=DB_HOTEL_4565.mssql.somee.com;persist security info=False;initial catalog=DB_HOTEL_4565";
+    }
+}
